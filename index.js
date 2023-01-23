6f5cf94b46376b6a4585dd830dd8025c154eb146
Ayushi Pandey <40225444+ayushipandey01@users.noreply.github.com>
2023-01-23 21:49:18 +0530

    items displayed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let todoInput = document.querySelector(".input-btn");
 
 let addTodoButton = document.querySelector(".button");
 
+let showTodos = document.querySelector(".todos-container");
+
 let todo;
 let todoList = [];
 
@@ -18,5 +20,19 @@ addTodoButton.addEventListener("click" , () => {
     if(todo.length > 0 ){
         todoList.push({id : uuid() , todo : todo , isCompleted : false})
     }
+    renderTodoList(todoList);
+})
+
+showTodos.addEventListener("click" , (e) =>{
+    let key = e.target.dataset.key;
+    // console.log(key);
+    // console.log(e.target);
+    todoList = todoList.map(todo => todo.id === key ? {...todo , isCompleted : !todo.isCompleted} : todo)
+    renderTodoList(todoList);
     console.log(todoList);
 })
+
+function renderTodoList(todoList){
+    showTodos.innerHTML = todoList.map(({id , todo ,isCompleted}) => `<div><input id ="item-${id}" type="checkbox" ${isCompleted ? "checked" : ""} data-key = ${id}><label for ="item-${id}" class ="todo todo-text t-pointer" ${isCompleted ? "checked-todo" : "" } data-key = ${id}>${todo}</label><button class="button">delete</button></div>`)
+}
+
